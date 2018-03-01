2fda4a22e4a33e4029f79eb663fd48910ed635a4
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-03-01 15:52:24 +0530

    Create holiday11.c

---
+++ holiday11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char a[20];
+    scanf("%s",&a);
+    if(a=="S"||a=="s")
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
